fe97643fae0b4d781148e4ad40432682560b7c7b
Dennis Kidake <denniskidake76@gmail.com>
2025-08-26 14:17:19 +0300

    setup deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'plp-mern-stack-development-classroom-week-3-react-js-assignment-MERN-Stack-Week3', // 👈 repo name
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
